b59ed2271108028cb3f4578141bf1fb9d0235387
Tony Buser <tbuser@gmail.com>
2008-06-23 18:33:40 -0400

    started working on a new graph to show velocity of change over time, but can't quite come up with a good way to do it, so commented it out

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
+class Array; def total; inject( nil ) { |total,x| total ? total+x : x }; end; end
+class Array; def mean; total / size; end; end
+
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
   def spinner(id)
     image_tag "spinner.gif", :id => id, :style => "display:none"
   end
@@ -22,11 +26,15 @@ module ApplicationHelper
     data_set_js     = ""
     data_set_names  = []
 
+    trend_data_sets       = {}
+    trend_data_set_count  = 0
+    trend_data_set_js     = ""
+    trend_data_set_names  = []
+
     events.each do |e|
       data_sets[e.sensor] ||= []
       data_sets[e.sensor] << [e.created_at.to_i, e.temp]
     end
-
 
     data_sets.each do |sensor,temps|
       temps_js      = ""
@@ -41,7 +49,7 @@ module ApplicationHelper
       data_set_js << "d#{data_set_count} = [#{temps_js}];\n    "
 
       data_parts_js << "data: d#{data_set_count}"
-      data_parts_js << "label: '#{sensor.name} #{sensor.temp}F'"
+      data_parts_js << "label: '#{sensor.name} #{temps[0][1]}F'"
 
       # data_parts_js << "lines: {fill: true}" if sensor.alarm == "Fire"
 
@@ -50,10 +58,48 @@ module ApplicationHelper
       data_set_count += 1
     end
 
+    data_sets.each do |sensor,temps|
+      trend_temp_js       = ""
+      trend_temp_strings  = []
+      trend_data_parts_js = []
+
+      change = 0
+      # average = temps.collect{|x| x[1].to_i}.mean
+      x = 1
+      
+      temps.each do |temp|
+        if x == 60
+          # change = temp[1] - average
+          change = temp[1]
+        
+          trend_temp_strings << "[#{temp[0]}, #{change}]"
+          
+          x = 1
+        else
+          x += 1
+        end
+      end
+      
+      trend_temps_js = trend_temp_strings.join(", ")
+      trend_data_set_js << "t#{trend_data_set_count} = [#{trend_temps_js}];\n    "
+
+      trend_data_parts_js << "data: t#{trend_data_set_count}"
+      trend_data_parts_js << "label: '#{sensor.name} #{change}F'"
+
+      trend_data_set_names << "{#{trend_data_parts_js.join(", ")}}"
+
+      trend_data_set_count += 1
+    end
+
     {
       :container_name => container_name,
       :data_set_js => data_set_js,
-      :data_set_names => data_set_names
+      :data_set_names => data_set_names,
+      :trend => {
+        :container_name => "trend_#{container_name}",
+        :data_set_js => trend_data_set_js,
+        :data_set_names => trend_data_set_names
+      }
     }
   end
   
@@ -67,6 +113,9 @@ module ApplicationHelper
     
     options[0][:type] = "Food"
     options[1][:type] = "Fire"
+
+    options[0][:trend][:type] = "Food_Trend"
+    options[1][:trend][:type] = "Fire_Trend"
     
     render :partial => "/shared/graphs", :locals => {:options => options}
   end
